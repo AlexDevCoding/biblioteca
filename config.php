847d507f3ec9cf895a8bfe9024671588c2abe202
Josue Alexander Henriquez <henriquezjosue384@gmail.com>
2024-08-08 10:16:32 -0400

    asdasd

--- config.php
+++ config.php
@@ -12,3 +12,23 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
+
+
+<?php
+$id = (int) $_GET['id']; 
+
+include("conexion_db.php");
+
+$sql = "DELETE FROM usuarios WHERE id=?";
+$stmt = mysqli_prepare($conexion, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+if (mysqli_stmt_execute($stmt)) {
+    echo "<script>alert('El usuario se eliminó correctamente'); location.href='lista-usuarios.php';</script>";
+} else {
+    echo "<script>alert('Error al eliminar el usuario: " . mysqli_error($conexion) . "'); location.href='lista-usuarios.php';</script>";
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($conexion);
+?>
