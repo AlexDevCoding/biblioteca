a7493aa663af1fe50d04cc9e4311d15c1816e5e4
Josue547 <henriquezjosue384@gmail.com>
2024-08-21 13:14:40 -0400

    prueba

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-08-2024 a las 17:11:07
+-- Tiempo de generación: 21-08-2024 a las 19:12:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
