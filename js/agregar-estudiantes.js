3b94288ec645ed9e1edf1c96f1ae68a73980dfae
Josue547 <henriquezjosue384@gmail.com>
2024-08-20 20:32:56 -0400

    comit final

--- js/agregar-estudiantes.js
+++ js/agregar-estudiantes.js
@@ -10,17 +10,17 @@ document.getElementById("formulario-registro").addEventListener("submit", functi
     .then(response => response.json())
     .then(data => {
         if (data.success) {
-            mostrarModal("Success", "Your account was registered!");
+            mostrarModal("Felicidades", "Su estudiante ha sido registrado!", "success");
         } else {
-            mostrarModal("Error", "There was an error. Please try again.");
+            mostrarModal("Inconveniente", "Hubo un error. Esta Cédula ya se encuentra registrada", "error");
         }
     })
     .catch(error => {
-        mostrarModal("Error", "There was an error. Please try again.");
+        mostrarModal("Inconveniente", "Hubo un Inconveniente. Esta Cédula ya se encuentra registrada", "error");
     });
 });
 
-function mostrarModal(title, message) {
+function mostrarModal(title, message, type) {
     const modal = document.getElementById("modal-registro-exitoso");
     const span = document.createElement("span");
     span.className = "cerrar";
@@ -28,8 +28,20 @@ function mostrarModal(title, message) {
 
     const modalContent = document.querySelector(".modal-contenido");
 
+
+    let icon = "";
+    let iconClass = "";
+
+    if (type === "success") {
+        icon = "check_circle";
+        iconClass = "icono-exito"; 
+    } else if (type === "error") {
+        icon = "error";
+        iconClass = "icono-error"; 
+    }
+
     modalContent.innerHTML = `
-        <div class="modal-icono"><span class="material-symbols-outlined">check_circle</span></div>
+        <div class="modal-icono ${iconClass}"><span class="material-symbols-outlined">${icon}</span></div>
         <div class="modal-texto">
             <h3>${title}</h3>
             <p>${message}</p>
